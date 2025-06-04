97634dec2bf9312f78e0642022f8e852ced5cd9a
gajendra-b2b <gajendra.sharma@greatlearning.in>
2025-06-05 02:18:36 +0530

    Update test_pipeline.py

--- challenge/src/tests/test_pipeline.py
+++ challenge/src/tests/test_pipeline.py
@@ -3,25 +3,32 @@ from pyspark.sql import SparkSession
 from main.job.pipeline import SalesAnalysis
 import os
 
-# Initialize Spark and job
 spark = SparkSession.builder.master("local").appName("Test Sales").getOrCreate()
+
 job = SalesAnalysis(spark)
 
 # Load test data
 DATA_PATH = "../../data/products.csv"
-df = job.load_data(DATA_PATH)
+def load_data(path):
+    return spark.read.csv(path, header=True, inferSchema=True)
+
+df = load_data(DATA_PATH)
 
 
 @pytest.mark.filterwarnings("ignore")
-def test_spark_session_creation():
+def test_init_spark_session():
     # Task 1: SparkSession should not be None
-    session = job.init_spark_session()
-    assert session is not None
+    job1 = SalesAnalysis(None)
+    session = job1.init_spark_session()
+    assert isinstance(session, SparkSession)
 
 @pytest.mark.filterwarnings("ignore")
 def test_load_data():
     # Task 2: Loaded DataFrame should not be empty
-    assert df.count() > 0
+    df = job.load_data(DATA_PATH)
+    assert df.count() == 50
+    assert "product" in df.columns
+
 @pytest.mark.filterwarnings("ignore")
 def test_display_schema():
     # Task 3: Schema should include expected fields
@@ -43,21 +50,30 @@ def test_total_products():
 @pytest.mark.filterwarnings("ignore")
 def test_top_n_products():
     # Task 6: Should return exactly 3 products
-    top3 = job.top_n_products(df, 3)
-    assert top3.count() == 3
+    top3 = job.top_n_products(df, 3).collect()
+    assert len(top3) == 3
+    assert top3[1]['product'] == "Product C"
 
 @pytest.mark.filterwarnings("ignore")
 def test_total_sales():
     # Task 7: Should return total sales > 0
     total = job.total_sales(df)
-    assert total > 0
+    assert total == 5767
 
 @pytest.mark.filterwarnings("ignore")
 def test_market_share():
-    # Task 8: Should return market share sum ≈ 100%
     market_df = job.market_share(df)
-    total_share = sum(row["market_share"] for row in market_df.collect())
-    assert abs(total_share - 100.0) < 1e-2
+    market_data = {row["product"]: round(row["market_share"], 2) for row in market_df.collect()}
+    
+    # Manually computed expected values based on 5627 total sales
+    expected_market_shares = {
+        "Product A": 28.81,
+        "Product B": 17.6,
+        "Product C": 26.48,
+        "Product D": 12.54,
+        "Product E": 14.86,
+    }
 
-
+    for product, expected_share in expected_market_shares.items():
+        assert market_data.get(product) == pytest.approx(expected_share, rel=1e-2)
 
