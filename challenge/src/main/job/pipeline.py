750b68cbb1cb7986433ce1ee4963c28db6c83cbf
gajendra-b2b <gajendra.sharma@greatlearning.in>
2025-10-01 16:26:54 +0530

    Update pipeline.py

--- challenge/src/main/job/pipeline.py
+++ challenge/src/main/job/pipeline.py
@@ -5,35 +5,9 @@ class SalesAnalysis:
     
     def __init__(self, spark: SparkSession):
         self.spark = spark
-    # Task 1: Create SparkSession for app "Sales Data Analysis"
-
-    def init_spark_session(self) -> SparkSession:
-        pass
-
-    # Task 2: Load CSV file from local storage as DataFrame
+        
+    # Task 1: Load CSV file from local storage as DataFrame
     def load_data(self, path: str) -> DataFrame:
         pass
+        # Write your code below
 
-    # Task 3: Verify schema of the DataFrame
-    def display_schema(self, df: DataFrame) -> None:
-        pass
-
-    # Task 4: Display entries for country "Germany"
-    def filter_by_country(self, df: DataFrame, country: str = "Germany") -> DataFrame:
-        pass
-
-    # Task 5: Find total number of unique products
-    def total_products(self, df: DataFrame) -> int:
-        pass
-
-    # Task 6: Find top N products by sales
-    def top_n_products(self, df: DataFrame, n: int) -> DataFrame:
-        pass
-
-    # Task 7: Calculate total sales across all products
-    def total_sales(self, df: DataFrame) -> int:
-        pass
-        
-    # Task 8: Find individual market share of all products
-    def market_share(self, df: DataFrame) -> DataFrame:
-        pass
